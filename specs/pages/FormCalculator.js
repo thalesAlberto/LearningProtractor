ba9d2751336d3cd1c3fcd1b310a5ef4643e6a88a
Thales Alberto <Thales Alberto>
2018-10-05 16:56:40 -0300

    Initial Commit

---
+++ specs/pages/FormCalculator.js
@@ -0,0 +1,24 @@
+import Page from "./page"
+
+class FormCalculator extends Page {
+
+    get firstCamp() { return element(by.model('first')) };
+    get secondCamp() { return element(by.model('second')) };
+    get operatorSelect() { return element(by.model('operator')) };
+    get button() { return element(by.id('gobutton')) };
+    get result() { return element(by.css('h2[class="ng-binding"]')).getText() };
+        
+    open() {
+        super.open('http://juliemr.github.io/protractor-demo/');
+    }
+    
+    submit() {
+        this.button.click();
+    }
+    
+    selectOperator(operator) {
+        return element(by.css("option[value = "+ `${operator}` + "]")).click();
+    }
+}
+
+export default new FormCalculator();
---
+++ specs/specs/calculator.spec.js
@@ -0,0 +1,46 @@
+import FormCalculator from '../pages/FormCalculator'
+
+describe('Testando a funcionalidade das operações matemáticas', () => {
+
+    //Abre o browser da aplicação
+    beforeAll(() => {
+        FormCalculator.open();
+    });
+
+    //Setando Valores e preparando para mudança de operação
+    beforeEach(() => {
+        FormCalculator.firstCamp.sendKeys(12);
+        FormCalculator.secondCamp.sendKeys(4);
+        FormCalculator.operatorSelect.click();
+    });
+
+    it('Deve retornar um valor correto na soma de dois inteiros', () => {
+        FormCalculator.submit();
+        expect(FormCalculator.result).toEqual('16');
+    });
+
+    it('Deve retornar um valor correto na subtração de dois inteiros', () => {
+        FormCalculator.selectOperator("SUBTRACTION");
+        FormCalculator.submit();
+        expect(FormCalculator.result).toEqual('8');
+    });
+
+    it('Deve retornar um valor correto na multiplicação de dois inteiros', () => {
+        FormCalculator.selectOperator("MULTIPLICATION");
+        FormCalculator.submit();
+        expect(FormCalculator.result).toEqual('48');
+    });
+
+    it('Deve retornar um valor correto na divisão de dois inteiros', () => {
+        FormCalculator.selectOperator("DIVISION");
+        FormCalculator.submit();
+        expect(FormCalculator.result).toEqual('3');
+    });
+
+    it('Deve retornar um valor correto no resto de divisão de dois inteiros', () => {
+        FormCalculator.selectOperator("MODULO");
+        FormCalculator.submit();
+        expect(FormCalculator.result).toEqual('0');
+    });
+
+});
